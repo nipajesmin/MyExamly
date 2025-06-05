0dd6819311a7d5a2eeec6491bb18117e9172d890
nipajesmin <nipakterjesmin@gmail.com>
2025-06-05 21:49:56 +0600

    add slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  darkMode: 'class',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
